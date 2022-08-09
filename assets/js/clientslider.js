32865c8658f5278a0d4d628f5283c12d8ffb3014
Arham-1003 <gandhiarham1003@gmail.com>
2022-08-09 23:30:26 +0530

    Major Updates

--- assets/js/clientslider.js
+++ assets/js/clientslider.js
@@ -1,8 +1,9 @@
  
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
+    slidesPerView: 4,
     spaceBetween: 25,
     loop: true,
+    autoplay: false,
     centerSlide: 'true',
     fade: 'true',
     grabCursor: 'true',
@@ -17,15 +18,23 @@ var swiper = new Swiper(".slide-content", {
     },
 
     breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+      1200:{
+          slidesPerView : 4
+      },
+
+      992:{
+          slidesPerView : 3
+      },
+      576:{
+          slidesPerView : 2
+      },
+
+      381:{
+          slidesPerView : 1
+      },
+      0:{
+        slidesPerView : 1
+      }
     },
   });
 
